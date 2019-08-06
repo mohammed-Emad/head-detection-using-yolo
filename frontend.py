82aa6598b694154ef1e87a1ab6ac4de9361d45fa
mohammed-Emad <mohammed.imad38@yahoo.com>
2019-08-06 21:11:55 +0200

    Update frontend.py

--- frontend.py
+++ frontend.py
@@ -5,7 +5,7 @@ import tensorflow as tf
 import numpy as np
 import os
 import cv2
-from utils import decode_netout, compute_overlap, compute_ap
+from utilsB import decode_netout, compute_overlap, compute_ap
 from keras.applications.mobilenet import MobileNet
 from keras.layers.merge import concatenate
 from keras.optimizers import SGD, Adam, RMSprop
